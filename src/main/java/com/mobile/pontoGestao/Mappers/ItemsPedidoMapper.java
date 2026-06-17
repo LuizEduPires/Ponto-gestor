51271c82465de2a238424b0f6fbaba47ec80449d
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2026-06-17 04:18:04 -0300

    fix(items): padroniza Timestamp e corrige mapeamento de ItemsPedido

--- src/main/java/com/mobile/pontoGestao/Mappers/ItemsPedidoMapper.java
+++ src/main/java/com/mobile/pontoGestao/Mappers/ItemsPedidoMapper.java
@@ -1,6 +1,5 @@
 package com.mobile.pontoGestao.Mappers;
 
-import com.google.cloud.Timestamp;
 import com.mobile.pontoGestao.Dtos.Request.ItemsPedidoRequest;
 import com.mobile.pontoGestao.Models.ItemsPedido;
 import org.mapstruct.Mapper;
@@ -9,13 +8,7 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface ItemsPedidoMapper {
 
-    @Mapping(target = "dataPrazo", expression = "java(toTimestamp(request.dataPrazo()))")
     @Mapping(target = "dataProva", ignore = true)
     @Mapping(target = "dataEntrega", ignore = true)
     ItemsPedido toModel(ItemsPedidoRequest request);
-
-    default Timestamp toTimestamp(Long millis) {
-        if (millis == null) return null;
-        return Timestamp.ofTimeMicroseconds(millis * 1000);
-    }
 }
