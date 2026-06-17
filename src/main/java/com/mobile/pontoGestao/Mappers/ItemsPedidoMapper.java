f3490e53d4ed89c5a87a91176d78f6988555d79b
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2026-06-17 04:10:04 -0300

    :bug: fix(mapper): corrige conversão de ItemsPedidoRequest para model

--- src/main/java/com/mobile/pontoGestao/Mappers/ItemsPedidoMapper.java
+++ src/main/java/com/mobile/pontoGestao/Mappers/ItemsPedidoMapper.java
@@ -1,24 +1,21 @@
 package com.mobile.pontoGestao.Mappers;
 
+import com.google.cloud.Timestamp;
 import com.mobile.pontoGestao.Dtos.Request.ItemsPedidoRequest;
-import com.mobile.pontoGestao.Dtos.Response.ItemsPedidoResponse;
 import com.mobile.pontoGestao.Models.ItemsPedido;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.Mapping;
 
-@Mapper(
-    componentModel = "spring",
-    nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
-)
+@Mapper(componentModel = "spring")
 public interface ItemsPedidoMapper {
 
+    @Mapping(target = "dataPrazo", expression = "java(toTimestamp(request.dataPrazo()))")
+    @Mapping(target = "dataProva", ignore = true)
+    @Mapping(target = "dataEntrega", ignore = true)
     ItemsPedido toModel(ItemsPedidoRequest request);
 
-    ItemsPedidoResponse toDto(ItemsPedido model);
-
-    void updateFromRequest(
-            ItemsPedidoRequest request,
-            @MappingTarget ItemsPedido item
-    );
+    default Timestamp toTimestamp(Long millis) {
+        if (millis == null) return null;
+        return Timestamp.ofTimeMicroseconds(millis * 1000);
+    }
 }
