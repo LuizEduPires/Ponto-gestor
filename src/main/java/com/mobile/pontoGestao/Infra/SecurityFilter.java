876a3bf4857fe9ef9e2067ecd795a6a6a89404a8
Lucas Flôres <124315388+LuxLucas@users.noreply.github.com>
2026-06-22 05:03:06 -0300

    Update SecurityFilter.java

--- src/main/java/com/mobile/pontoGestao/Infra/SecurityFilter.java
+++ src/main/java/com/mobile/pontoGestao/Infra/SecurityFilter.java
@@ -9,6 +9,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -28,6 +30,12 @@ public class SecurityFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain
     ) throws ServletException, IOException {
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         String token = recoverToken(request);
 
